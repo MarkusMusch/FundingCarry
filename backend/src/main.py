4a0c994f7d9be60da105e4cf12b4dc62523829b2
Markus <markus-musch@web.de>
2025-03-02 19:38:04 +0100

    add chart endpoint backend

--- backend/src/main.py
+++ backend/src/main.py
@@ -16,6 +16,23 @@ def read_root():
     response = {"message": "Hello World!"}
     return response
 
+@app.get("/chart")
+def read_chart():
+    response = {
+        "data": [
+            { "date": '2025-03-01', "Apples": 400, "Oranges": 300 },
+            { "date": '2025-03-02', "Apples": 300, "Oranges": 500 },
+            { "date": '2025-03-03', "Apples": 450, "Oranges": 350 },
+            { "date": '2025-03-04', "Apples": 500, "Oranges": 400 }
+        ],
+        "dataKey": "date",
+        "series":[
+            { "name": 'Apples', "color": '#1E90FF' },
+            { "name": 'Oranges', "color": '#FFA500' },
+        ]
+    }
+    return response
+
 
 if __name__ == "__main__":
    import uvicorn
